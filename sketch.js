a7a6becc7575b29f3f7c976da79d233e6d6d8da9
TimAde18 <aoadewole18@gmail.com>
2023-09-08 07:43:40 -0500

    feat: Enabled four of the seven buttons

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 
 const gridContainer = document.getElementById("grid-container");
-//function for 16 x 16 board 
+//function for grid board 
 function gridBoard (rowNum, colNum) {
+    gridContainer.innerHTML = "";
+    gridContainer.style.gridTemplateColumns = `repeat(${colNum}, 1fr)`;
     for(let i=0; i< rowNum; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("row");
@@ -10,26 +12,65 @@ function gridBoard (rowNum, colNum) {
             const gridCell = document.createElement("div");
             gridCell.classList.add("cell");
             gridRow.appendChild(gridCell);
+            
         }
     }
 };
+//gridBoard(16, 16);
 
-gridBoard(16, 16);
+//function for size button
+let numberOfCell = 0;
+let grids = document.querySelectorAll(".cell");
+
+function gridSize (num) {
+    if (numberOfCell != num) {
+        gridBoard(num, num);
+        grids = document.querySelectorAll(".cell");
+        console.log(grids)
+        numberOfCell = num;
+    } else {
+        alert("You have chosen this already");
+    }
+    resetGrid();
+}
+console.log(numberOfCell);
+
+
+const smallGridBtn = document.getElementById("16");
+smallGridBtn.addEventListener("click", function () {
+    gridSize(16);
+});
+
+const midGridBtn = document.getElementById("32");
+midGridBtn.addEventListener("click", function () {
+    gridSize(32);
+});
 
 //function for hovering over cells
 
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
-
-function mouseStencil (color) {
-    const grids = document.querySelectorAll(".cell");
-    gridContainer.addEventListener("mouseover", (e) => {
-        if(mouseDown) {
-            const target = e.target;
-            target.style.backgroundColor = color;
-        }
+function mousePencil (color) {
+    console.log(grids)
+    grids.forEach((cell) => {
+        cell.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = color;
+        });
     })
 };
 
-mouseStencil("black");
+//function for color buttons
+const blackBtn = document.getElementById("black");
+blackBtn.addEventListener("click", function () {
+    mousePencil("black");
+});
+
+
+//function for reset
+function resetGrid() {
+    console.log(grids)
+    grids.forEach((cell) => {
+        cell.style.backgroundColor = "white";
+    });
+}
+
+const resetBtn = document.getElementById("reset");
+resetBtn.addEventListener("click", resetGrid);
